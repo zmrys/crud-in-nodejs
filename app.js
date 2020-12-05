b9bc87573d26a4ecf5580c003561c0a9401406db
Zin Min Tun <zinmintuncu@gmail.com>
2020-12-05 23:47:22 +0630

    Config:Add logging middleware,morgan.

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan'); //logging middleware
+
 const app = express();
 
-app.route("/hello").get((req, res, next) => {
+if (process.env.NODE_DEV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.route('/hello').get((req, res, next) => {
   res.status(200).json({
-    status: "ok",
-    message: "This is my first route.",
+    status: 'ok',
+    message: 'This is my first route.',
   });
-  next();
 });
 
 //Handling undefined routes
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   res.status(404).json(`Can't find${req.originalUrl}`);
   //next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-const app = require("./app");
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
 const port = process.env.PORT;
 
 const server = app.listen(port, () => {
